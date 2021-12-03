fd51b42510584ea1adb958eac07d939551257eeb
ackmp <ackmp@pm.me>
2021-12-03 14:47:19 +0100

    Add day 3 part 2 solution

--- src/main/scala/com/ackmp/adventofcode/year2021/Day03.scala
+++ src/main/scala/com/ackmp/adventofcode/year2021/Day03.scala
@@ -1,19 +1,35 @@
 package com.ackmp.adventofcode.year2021
 
 object Day03 {
-  def getPowerConsumption(input: List[List[String]]): Any = {
-    val gammaRate = input
+  // part 1
+
+  def toDec(str: String): Int = Integer.parseInt(str, 2)
+
+  def getGammaRate(input: List[List[String]]): String = input
       .transpose
       .map(_.count(_ == "1"))
-      .map(x => if (x > input.length / 2) "1" else "0")
+      .map(x => if (x >= input.length.toFloat / 2) "1" else "0")
       .mkString("")
-
-    val epsilonRate = gammaRate
+    
+  def getEpsilonRate(input: List[List[String]]): String = getGammaRate(input)
       .split("")
       .map(x => if (x == "1") "0" else "1")
       .mkString("")
 
-    Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2)
+  def getPowerConsumption(input: List[List[String]]): Any = {
+    toDec(getGammaRate(input)) * toDec(getEpsilonRate(input))
+  }
+
+  // part 2
+  def getLifeSupportRating(input: List[List[String]]): Any = {
+    def getRating(input: List[List[String]], n: Int, rateType: (List[List[String]]) => String,rates: List[String] = List()): String = input match {
+      case x if x.length == 1 => x.head.mkString
+      case _ => {
+        val newRates = rateType(input).split("").toList
+        getRating(input.filter(x => x(x.length - n) == newRates(newRates.length - n)), n - 1, rateType, newRates)
+      }
+    }
+    toDec(getRating(input, input.head.length, getGammaRate)) * toDec(getRating(input, input.head.length, getEpsilonRate))
   }
 
   lazy val input: List[List[String]] = io.Source
@@ -23,5 +39,6 @@ object Day03 {
 
   def main(args: Array[String]): Unit = {
     println(getPowerConsumption(input))
+    println(getLifeSupportRating(input))
   }
 }
