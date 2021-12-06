8a00e7fdb12be156fce09533187b36eea7231090
Marcin <marcin@macbook-4c1k.home>
2021-12-06 22:23:34 +0100

    Add day 6 faster solution

--- src/main/scala/com/ackmp/adventofcode/year2021/Day06.scala
+++ src/main/scala/com/ackmp/adventofcode/year2021/Day06.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 object Day06 {
   @tailrec
-  def doCycle(fishes: List[Int], days: Int): Any = days match {
+  def doCycle(fishes: List[Int], days: Int): Int = days match {
     case 0 => fishes.length
     case n =>
       doCycle(
@@ -19,6 +19,18 @@ object Day06 {
       )
   }
 
+  def createTimers(fishes: List[Int]): List[Long] = {
+    fishes.foldLeft(List.fill(9)(0): List[Long])((acc, t) => acc.updated(t, acc(t) + 1))
+  }
+
+  def fastCycle(timers: List[Long], days: Int): Long = {
+    if (days == 0) timers.sum
+    else timers match {
+      case List(t0, t1, t2, t3, t4, t5, t6, t7, t8) =>
+        fastCycle(List(t1, t2, t3, t4, t5, t6, t7 + t0, t8, t0), days - 1)
+    }
+  }
+
   lazy val input: List[Int] = io.Source
     .fromInputStream(getClass.getResourceAsStream("Day06.txt")).getLines
     .flatMap(_.split(",").map(_.toInt))
@@ -26,5 +38,6 @@ object Day06 {
 
   def main(args: Array[String]): Unit = {
     println(doCycle(input, 80))
+    println(fastCycle(createTimers(input), 256))
   }
 }
